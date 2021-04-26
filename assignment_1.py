9d2f2dc99c07e74bd0d37e2cc27ad45478c2352e
Toludoyin <toludoyinshopein@gmail.com>
2021-04-26 17:24:59 +0100

    creating assignment

---
+++ assignment_1.py
@@ -0,0 +1,48 @@
+#function
+def subtract():
+    subtraction = 12-4
+    print(subtraction)
+subtract()
+
+def divide():
+    division = 100/2
+    print(division)
+divide()
+
+
+def multiply(a,b):
+    print(a*b)
+multiply(3,7)
+
+
+#function with parameter
+def square(value):
+    new_square = value **4
+    print(new_square)
+square(6)
+square(8)
+
+
+#using return keyword
+def sum(fig):
+    new_sum = fig +4
+    return new_sum
+num=sum(13)
+print(num)
+
+
+def sum_of_two_num(x,y):
+    print('the addition of the number is approximately %s and %s'%(x,y))
+sum_of_two_num(4,6)
+
+
+
+
+
+
+
+
+
+
+
+
